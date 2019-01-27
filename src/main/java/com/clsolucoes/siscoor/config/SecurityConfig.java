947cc1cef7b83eb9bc55e153b333b2bbb19f8ab3
Silva <jrjrsilva@gmail.com>
2019-01-27 12:18:24 -0300

    Ajustes para exibir o layout

--- src/main/java/com/clsolucoes/siscoor/config/SecurityConfig.java
+++ src/main/java/com/clsolucoes/siscoor/config/SecurityConfig.java
@@ -38,7 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
-			"/h2-console/**"
+			"/h2-console/**",
+			"/resources/**",
+			"/layout/**",
+			"/stylesheets/**",
+			"/javascripts/**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
@@ -62,6 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+			.antMatchers("/resoruces/**").permitAll()
 			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
